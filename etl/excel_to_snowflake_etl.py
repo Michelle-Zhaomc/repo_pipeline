8a17ab3d5e539f17a3d88a8d4e26f9aad2788936
Michelle-Zhaomc <zhaomei@sheridancollege.ca>
2025-01-23 21:12:19 -0500

    update code

--- etl/excel_to_snowflake_etl.py
+++ etl/excel_to_snowflake_etl.py
@@ -49,37 +49,42 @@ def excel_to_snowflake_etl():
         # sheets = pd.ExcelFile(excel_file)  
         # Step 1: Get all sheet names using Pandas
         excel_file = pd.ExcelFile(excel_file)
+        # Meichao commented
+        #sheet_names = excel_file.sheet_names
         sheet_names = excel_file.sheet_names
+        if len(sheet_names) == 1:
+            sheet_name = sheet_names[0]
 
         # Step 2: Load each sheet into a Spark DataFrame
         spark_dfs = {}
-        for sheet_name in sheet_names:
-            print(f"Loading sheet: {sheet_name}")
-            spark_df = spark.read.format("com.crealytics.spark.excel") \
-                .option("header", "true") \
-                .option("inferSchema", "true") \
-                .option("dataAddress", f"'{sheet_name}'!A1") \
-                .option("maxRowsInMemory", 20000) \
-                .load(github_url)
-            for col in spark_df.columns:
-                spark_df = spark_df.withColumnRenamed(col, col.replace(' ', '_'))
+        # Meichao comment
+        #for sheet_name in sheet_names:
+        print(f"Loading sheet: {sheet_name}")
+        spark_df = spark.read.format("com.crealytics.spark.excel") \
+            .option("header", "true") \
+            .option("inferSchema", "true") \
+            .option("dataAddress", f"'{sheet_name}'!A1") \
+            .option("maxRowsInMemory", 20000) \
+            .load(github_url)
+        for col in spark_df.columns:
+            spark_df = spark_df.withColumnRenamed(col, col.replace(' ', '_'))
             # Add the DataFrame to a dictionary with the sheet name as the key
-            spark_dfs[sheet_name] = spark_df
-            print(f"Loaded {sheet_name} with {spark_df.count()} rows")
+        spark_dfs[sheet_name] = spark_df
+        print(f"Loaded {sheet_name} with {spark_df.count()} rows")
             
             # Define Snowflake table name (based on sheet name)
-            table_name = sheet_name.replace(" ", "_")
-            spark_df.show()
-            print(snowflake_options,table_name)
+        table_name = sheet_name.replace(" ", "_")
+        spark_df.show()
+        print(snowflake_options,table_name)
             # Write data to Snowflake
-            spark_df.write \
-                    .format("snowflake") \
-                    .options(**snowflake_options) \
-                    .option("dbtable", table_name) \
-                    .mode("overwrite") \
-                    .save()
+        spark_df.write \
+                .format("snowflake") \
+                .options(**snowflake_options) \
+                .option("dbtable", table_name) \
+                .mode("overwrite") \
+                .save()
                 
-            print(f"Data written to Snowflake table '{table_name}'")
+        print(f"Data written to Snowflake table '{table_name}'")
 
     # Load and write the AdventureWorks data from an Excel file to Snowflake
     # excel_file_path = excel_path  # Use raw string
